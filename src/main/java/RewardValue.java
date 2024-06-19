0f8f72e66874d590f7c9ade6efa304508b25551b
PruthibirajNayak <pruthibirajn@gmail.com>
2024-06-19 16:52:12 +0530

    Merge remote-tracking branch 'origin/reward-value' into reward-value

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,10 +1,15 @@
 public class RewardValue {
-    public double cashValue;
-    public double milesValue;
+     double cashValue;
+     int milesValue;
     // Constructor that accepts a cash value
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
-        this.milesValue = convertToMiles(cashValue);
+        this.milesValue = (int)(cashValue/0.0035);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue*0.0035;
     }
 
     // Method to get the cash value of the RewardValue
@@ -16,10 +21,5 @@ public class RewardValue {
     public double getMilesValue() {
         return milesValue;
     }
-
-    // Private method to convert cash value to miles
-    private double convertToMiles(double cashValue) {
-        return cashValue/0.0035;
-}
 
 }
